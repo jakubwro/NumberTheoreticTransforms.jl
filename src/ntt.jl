86652f5d43b315c091aa88f76c58b6144d91f48b
Jakub Wronowski <jakubwro@gmail.com>
2019-12-13 10:55:41 +0100

    latex and md in docstring try

--- src/ntt.jl
+++ src/ntt.jl
@@ -18,17 +18,32 @@
 export ntt, intt
 
 """
-    ntt(g, q, x)
+    ntt(g, q, x::Array{T,1}) -> Array{T,1}
 
-Number Theoretic Transform implementation directly from the formula.
-It is flexible about choosing transformation params but lacks performance.   
+The [Number Theoretic Transform](https://en.wikipedia.org/wiki/Discrete_Fourier_transform_(general)#Number-theoretic_transform)
+transforms data in a similar fashion to DFT, but instead complex roots of unity
+it uses integer roots with all operation defined in a finite field (modulo an
+integer number).
+
+`ntt` function implements Number Theoretic Transform directly from the formula.
 
 ``\\bar{x}_k = \\sum_{n=1}^N{x_n g^{(n-1)(k-1)} } \\mod q``
 
+It is flexible about choosing transformation params but lacks performance.
+
+There is alse few constraints on choosing parameteres and input length to
+ensure that inverse exists and equals to original input. (TODO: list constraints)
+
+The arguments of `ntt` function are
+
+-   `g`: transform exponential, must have inversion modulo q 
+-   `q`: transform calculations are done modulo q
+-   `x`: input data its elements must be smaller than q
 """
-function ntt(g::T, q::T, x::Array{T,1}) where {T <: Integer}
+function ntt(g::T, q::T, x::Array{T,1}) where {T<:Integer}
     N = length(x)
     #TODO: more validation of p,q, decompose it to struct
+    #TODO: create transform object that validates input in the constructor
     @assert mod(q - 1, N) == 0 
     @assert powermod(g, N, q) == 1
     @assert !(1 in powermod.(g, 2:N-1, q))
@@ -39,41 +54,7 @@ function ntt(g::T, q::T, x::Array{T,1}) where {T <: Integer}
     return mod.(t * x, q)
 end
 
-"""
-    intt(g, q, y)
-
-Inverse Number Theoretic Transform implementation directly from the formula.
-
-``x_k = N^{-1} \\sum_{n=1}^N{\\bar{x}_n g^{-(n-1)(k-1)} } \\mod q``
-
-"""
-function intt(g::T, q::T, y::Array{T,1}) where {T <: Integer}
-    N = length(y)
-    @assert mod(q - 1, N) == 0
-    @assert powermod(g, N, q) == 1
-    @assert !(1 in powermod.(g, 2:N-1, q))
-    @assert gcd(length(y), q) == 1
-    
-    inv_g = invmod(g, q)
-    inv_N = invmod(N, q)
-    t = [powermod(inv_g, l * k, q) for k in 0:N-1, l in 0:N-1]
-    return mod.(inv_N * t * y, q)
-end
-
-# those implementations are incorrect but transform results were interesing anyway
-# TODO: check if those calculations are useful
-
-# function ntt(g::T, q::T, x::Array{T,N}) where {T <: Integer,N}
-#     return reshape(ntt(g, q, reshape(x, length(x))), size(x))
-# end
-
-# function intt(g::T, q::T, y::Array{T,N}) where {T <: Integer,N}
-#     return reshape(intt(g, q, reshape(y, length(y))), size(y))
-# end
-
-# TODO: change implementation to handle any number of dimensions
-# it can be done calling N-1 dimensional transform in a loop
-function ntt(g::T, q::T, x::Array{T,2}) where {T <: Integer}
+function ntt(g::T, q::T, x::Array{T,2}) where {T<:Integer}
     N, M = size(x)
     @assert N == M #TODO: make it worl for N != M (need different g for each dim)
     y = zeros(T, size(x))
@@ -89,7 +70,29 @@ function ntt(g::T, q::T, x::Array{T,2}) where {T <: Integer}
     return y
 end
 
-function intt(g::T, q::T, y::Array{T,2}) where {T <: Integer}
+"""
+    intt(g, q, y::Array{T,1}) -> Array{T,1}
+
+Inverse Number Theoretic Transform implementation directly from the formula.
+
+``x_k = N^{-1} \\sum_{n=1}^N{\\bar{x}_n g^{-(n-1)(k-1)} } \\mod q``
+
+The same input parameters constraints as for `ntt` function must be applied
+"""
+function intt(g::T, q::T, y::Array{T,1}) where {T<:Integer}
+    N = length(y)
+    @assert mod(q - 1, N) == 0
+    @assert powermod(g, N, q) == 1
+    @assert !(1 in powermod.(g, 2:N-1, q))
+    @assert gcd(length(y), q) == 1
+    
+    inv_g = invmod(g, q)
+    inv_N = invmod(N, q)
+    t = [powermod(inv_g, l * k, q) for k in 0:N-1, l in 0:N-1]
+    return mod.(inv_N * t * y, q)
+end
+
+function intt(g::T, q::T, y::Array{T,2}) where {T<:Integer}
     N, M = size(y)
     x = zeros(T, size(y))
 
@@ -103,3 +106,19 @@ function intt(g::T, q::T, y::Array{T,2}) where {T <: Integer}
     
     return x
 end
+
+
+# those implementations are incorrect but transform results were interesing anyway
+# TODO: check if those calculations are useful
+
+# function ntt(g::T, q::T, x::Array{T,N}) where {T <: Integer,N}
+#     return reshape(ntt(g, q, reshape(x, length(x))), size(x))
+# end
+
+# function intt(g::T, q::T, y::Array{T,N}) where {T <: Integer,N}
+#     return reshape(intt(g, q, reshape(y, length(y))), size(y))
+# end
+
+# TODO: change implementation to handle any number of dimensions
+# it can be done calling N-1 dimensional transform in a loop
+
