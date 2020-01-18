5fe30ef56062a6cd9eb80e5ade26f1800d8e1d43
jakubwro <jakubwro@gmail.com>
2020-01-18 02:24:27 +0100

    script refactoring

--- scripts/find-ntt-methods.jl
+++ scripts/find-ntt-methods.jl
@@ -1,4 +1,22 @@
-function find_ntt_for_q(q)
+struct FindNTT{G<:Union{BigInt, Nothing}, Q<:Union{BigInt, Nothing}, N<:Union{Int, Nothing}}
+    g::G
+    q::Q
+    n::N
+end
+
+function (params::FindNTT{Nothing, Nothing, Int})()
+    n = params.n
+    for q in primes(n, 1000000n)
+        for g in 2:min(q, 100)
+            if mod(q - 1, n) == 0 && powermod(g, n, q) == 1 && !(1 in powermod.(g, 2:n - 1, q))
+                println("(g, q, n) = ($g, $q, $n)")
+            end
+        end
+    end
+end
+
+function (params::FindNTT{Nothing, BigInt, Nothing})()
+    q = params.q
     for g in 2:q
         for n in 1:q
             if powermod(g, n, q) == 1
@@ -9,16 +27,8 @@ function find_ntt_for_q(q)
     end
 end
 
-function find_ntt_for_q_n(q, n)
-    for g in 2:q
-        if powermod(g, n, q) == 1
-            println("(g, q, n) = ($g, $q, $n)")
-            break
-        end
-    end
-end
-
-function find_ntt_for_g_q(g, q)
+function (params::FindNTT{BigInt, BigInt, Nothing})()
+    (g, q) = (params.g, params.q)
     @assert g >= 0
     @assert g < q
     for n in 1:q
@@ -29,18 +39,19 @@ function find_ntt_for_g_q(g, q)
     end
 end
 
-function find_ntt_for_g_q_n(g, q, n)
-    if mod(q - 1, n) == 0 && powermod(g, n, q) == 1 && !(1 in powermod.(g, 2:n - 1, q))
-        println("(g, q, n) = ($g, $q, $n)")
+function (params::FindNTT{Nothing, BigInt, Int})()
+    (q, n) = (params.q, params.n)
+    for g in 2:q
+        if powermod(g, n, q) == 1
+            println("(g, q, n) = ($g, $q, $n)")
+            break
+        end
     end
 end
 
-function find_ntt_for_n(n)
-    for q in primes(n, 1000000n)
-        for g in 2:min(q, 100)
-            if mod(q - 1, n) == 0 && powermod(g, n, q) == 1 && !(1 in powermod.(g, 2:n - 1, q))
-                println("(g, q, n) = ($g, $q, $n)")
-            end
-        end
+function (params::FindNTT{BigInt, BigInt, Int})()
+    g, q, n = params.g, params.q, params.n
+    if mod(q - 1, n) == 0 && powermod(g, n, q) == 1 && !(1 in powermod.(g, 2:n - 1, q))
+        println("(g, q, n) = ($g, $q, $n)")
     end
 end
--- scripts/find-ntt.jl
+++ scripts/find-ntt.jl
@@ -1,38 +1,23 @@
 doc = """NTT parameter finder.
 
 Usage:
-  ntt-params.jl --modulo <q> [ --base <g> ]
-  ntt-params.jl --length <n> [ --modulo <q> ] [ --base <g> ]
+  ntt-params.jl --modulus <q> [ --base <g> ]
+  ntt-params.jl --length <n> [ --modulus <q> ] [ --base <g> ]
   ntt-params.jl -h | --help
 
 Options:
   -h --help     Show this screen.
   -n --length   Length of input vector.
-  -q --modulo   Modulo arithmetic.
+  -q --modulus  Modulo arithmetic.
   -g --base     Transform exponential base.
 """
 
 using DocOpt, Primes
 
 args = docopt(doc, version=v"2.0.0")
+args_parse(T, key) = args[key] === nothing ? nothing : parse(T, args[key])
+(g, q, n) = args_parse(BigInt, "<g>"), args_parse(BigInt, "<q>"), args_parse(Int64, "<n>")
 
 include("find-ntt-methods.jl")
-
-if args["--length"] && args["--modulo"] && args["--base"]
-    @show (g, q, n) = (parse(BigInt, args["<g>"]), parse(BigInt, args["<q>"]), parse(Int64, args["<n>"]))
-    find_ntt_for_g_q_n(g,q,n)
-elseif args["--length"] && args["--modulo"]
-    @show (q, n) = (parse(BigInt, args["<q>"]), parse(Int64, args["<n>"]))
-    find_ntt_for_q_n(q,n)
-elseif args["--length"]
-    @show n = parse(Int64, args["<n>"])
-    find_ntt_for_n(n)
-elseif args["--modulo"] && args["--base"]
-    @show (g, q) = (parse(BigInt, args["<g>"]), parse(BigInt, args["<q>"]))
-    find_ntt_for_g_q(g,q)
-elseif args["--modulo"]
-    @show q = parse(BigInt, args["<q>"])
-    find_ntt_for_q(q)
-else
-    println(doc)
-end
+findntt = FindNTT(g, q, n)
+findntt()
